729b414e82f76392dc6825d3ffdba35f83cf4d22
Kuramonni <armi.lampen@outlook.com>
2024-04-03 20:37:57 +0300

    Added privacy policy

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer>
     <hr>
     <p style="text-align: center;">&#169; 2024 Minä Itse</p>
+    <p>Please read about our <a href="privacypolicy.php">Privacy Policy</a></p>
 </footer>
 </body>
 </html>
---
+++ privacypolicy.php
@@ -0,0 +1,36 @@
+<!-- INCLUDE VS INCLUDE_ONCE: 
+include will add the specified file every time it is called. However, include_once will only ever include the file once and never again. -->
+<?php include_once "header.php"; ?> 
+
+
+<!-- Homepage introduction and img -->
+<div class="privacypolicy">
+    <h1>Privacy Policy</h1>
+    <p>
+Welcome to The Gym App. Before you proceed to utilize our services, it's important for you to understand our data handling practices as they relate to the General Data Protection Regulation (GDPR). By using our website and services, you acknowledge that you have read, understood, and agree to the terms outlined in this disclaimer.</p><br>
+
+<p><b>1. Collection of Personal Data:</b>
+When you create an account on our platform, we collect personal information including your name, email address, username, and password. This information is necessary for the creation and management of your account.</p><br>
+
+<p><b>2. Purpose of Data Collection:</b>
+We collect this information for the purpose of providing you with access to our services, including but not limited to our login system and exercise tracker. Your personal data is used solely for the functionality of our platform and is not shared with any third parties without your explicit consent.</p><br>
+<p><b>3. Exercise Tracker Data:</b>
+Our exercise tracker allows you to log gym sessions, including the date, exercise move, weights, and reps. This data is stored securely in our database and is only accessible to you through your account. We do not use this data for any purpose other than providing you with the functionality of our exercise tracker.</p><br>
+
+<p><b>4. User Rights:</b>
+As a user of our platform, you have the right to access, modify, and remove your personal information at any time. You can update your user information or delete your account directly through our website. Please note that deleting your account will also result in the removal of any data you have input into our exercise tracker database.</p><br>
+
+<p><b>5. Data Security:</b>
+We take the security of your personal information seriously and have implemented measures to protect it from unauthorized access, disclosure, alteration, or destruction. However, no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.</p><br>
+
+<p><b>6. Consent:</b>
+By using our website and services, you consent to the collection and use of your personal information as outlined in this disclaimer. If you do not agree with any part of this disclaimer, please refrain from using our website and services.</p><br>
+
+<p><b>7. Contact Information:</b>
+If you have any questions, concerns, or requests regarding your personal data or this disclaimer, please contact us at [contact email or form].</p><br>
+
+<p>By continuing to use our website and services, you acknowledge that you have read and understood this GDPR-compliant disclaimer. We reserve the right to update or modify this disclaimer at any time, so please review it periodically for changes.</p>
+</div>
+
+<?php include_once "footer.php"; ?>
+
